b688c222ca24341aadc058c9dde75e51aaddcb99
kariboter <kariboter1@gmail.com>
2023-06-22 21:42:38 +0300

    fixed requirments.txt

--- product_detecting.py
+++ product_detecting.py
@@ -1,11 +1,10 @@
-# from torchvision import transforms
-import torch
 import cv2
+import torch
 
-args = {'embeddings_path':'embeddings',
-        'model_path':'model'}
-mean=[0.485, 0.456, 0.406]
-std=[0.229, 0.224, 0.225]
+args = {'embeddings_path': 'embeddings',
+        'model_path': 'model'}
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
 
 
 def center_crop(img, dim):
@@ -15,14 +14,14 @@ def center_crop(img, dim):
     dim: dimensions (width, height) to be cropped
     """
     width, height = img.shape[1], img.shape[0]
-
     # process crop width and height for max available dimension
-    crop_width = dim[0] if dim[0]<img.shape[1] else img.shape[1]
-    crop_height = dim[1] if dim[1]<img.shape[0] else img.shape[0]
-    mid_x, mid_y = int(width/2), int(height/2)
-    cw2, ch2 = int(crop_width/2), int(crop_height/2)
-    crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
+    crop_width = dim[0] if dim[0] < img.shape[1] else img.shape[1]
+    crop_height = dim[1] if dim[1] < img.shape[0] else img.shape[0]
+    mid_x, mid_y = int(width / 2), int(height / 2)
+    cw2, ch2 = int(crop_width / 2), int(crop_height / 2)
+    crop_img = img[mid_y - ch2:mid_y + ch2, mid_x - cw2:mid_x + cw2]
     return crop_img
+
 
 def preprocess(img):
     """Returns resized and normalized image
@@ -32,8 +31,7 @@ def preprocess(img):
     img = cv2.resize(img, (256, 256))
     # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = center_crop(img, (254, 254))
-    img = img/255.0
-
+    img = img / 255.0
 
     img[..., 0] -= mean[0]
     img[..., 1] -= mean[1]
@@ -44,22 +42,24 @@ def preprocess(img):
     img[..., 2] /= std[2]
     return img
 
-def main(args:dict):
+
+def main(args: dict):
     cap = cv2.VideoCapture(0)
 
     model = torch.load(args['model_path'])
     model.to('cuda').eval()
-    embeddings = torch.load(args['embeddings_path'])      # embedding for comparing
+    embeddings = torch.load(args['embeddings_path'])  # embedding for comparing
 
     pdist = torch.nn.PairwiseDistance(p=4)
-    while(cap.isOpened()):
+    while (cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
             inputs = preprocess(frame)
-            tensor_input = torch.Tensor(inputs).view(3, 254, 254) # resizing image for torch model
+            tensor_input = torch.Tensor(inputs).view(3, 254, 254)  # resizing image for torch model
             with torch.no_grad():
-                curr_embedding = model(tensor_input[None,...].to('cuda')).to('cpu')
-            dists = {label:sum([pdist(curr_embedding, emb) for emb in embeddings[label]]) / len(embeddings[label]) for label in embeddings.keys()}
+                curr_embedding = model(tensor_input[None, ...].to('cuda')).to('cpu')
+            dists = {label: sum([pdist(curr_embedding, emb) for emb in embeddings[label]]) / len(embeddings[label]) for
+                     label in embeddings.keys()}
             min_dist = min(dists.values())
             top_label = [label for label in dists if dists[label] == min_dist]
             print(top_label)
